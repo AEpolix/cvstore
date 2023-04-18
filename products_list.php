927203436ba73d1c47c90c51e83b214d311761ee
AEpolix <viktoria.petrilakova@gmail.com>
2023-04-18 13:08:55 +0200

    prpojenie do databazy

---
+++ products_list.php
@@ -0,0 +1,22 @@
+<?php
+
+$servername = "localhost:3306";
+$username = "petrilakova";
+$password_db = "987654321";
+$database = "cvstore";
+
+try {
+
+    $connect = new PDO("mysql:host=$servername;dbname=$database", $username, $password_db);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // set the PDO error mode to exception
+    // echo "Connected Successfully";
+
+} catch(PDOException $e) {
+
+    echo "Connection Failed" .$e->getMessage();
+}
+
+?>
+
+
+
